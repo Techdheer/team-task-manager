81a821a4b839f0525fc4d88eb26c8613ef5cc72d
Dheerendra <singhdheerendra0110@gmail.com>
2026-05-20 15:27:35 +0530

    Added User model

--- backend/Models/User.js
+++ backend/Models/User.js
@@ -2,23 +2,30 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema(
   {
-    name: { type: String, required: true },
-
-    email: { type: String, required: true, unique: true },
-
-    password: { type: String, required: true },
-role: {
-  type: String,
-  enum: ["admin", "member"],
-  default: "member",
-},
-
-    profileImage: {
+    name: {
       type: String,
-      default: "",
+      required: true,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+
+    password: {
+      type: String,
+      required: true,
+    },
+
+    role: {
+      type: String,
+      default: "member",
     },
   },
-  { timestamps: true }
+  {
+    timestamps: true,
+  }
 );
 
 module.exports = mongoose.model("User", userSchema);
