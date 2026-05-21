ce2cb7764498aaa6c90acd8df42dcf9c5c53e1e1
Dheerendra <singhdheerendra0110@gmail.com>
2026-05-21 10:58:30 +0530

    Add all frontend routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,9 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Dashboard from "./pages/Dashboard";
+import Projects from "./pages/Projects";
+import Tasks from "./pages/Tasks";
+import KanbanBoard from "./pages/KanbanBoard";
 
 function App() {
   return (
@@ -11,6 +14,9 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/tasks" element={<Tasks />} />
+        <Route path="/kanban" element={<KanbanBoard />} />
       </Routes>
     </BrowserRouter>
   );
