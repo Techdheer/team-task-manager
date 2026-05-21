4eaa50e4d414658073f543dcf748d51f7625179e
Dheerendra <singhdheerendra0110@gmail.com>
2026-05-21 11:25:14 +0530

    Create tasks page

---
+++ frontend/src/pages/Tasks.jsx
@@ -0,0 +1,15 @@
+function Tasks() {
+  return (
+    <div className="p-10">
+      <h1 className="text-4xl font-bold">
+        Tasks Page
+      </h1>
+
+      <p className="mt-5">
+        Task management coming soon...
+      </p>
+    </div>
+  );
+}
+
+export default Tasks;
