389460b43508d0f6691e9499e5997a6b6ccaaf18
Dheerendra <singhdheerendra0110@gmail.com>
2026-05-21 11:27:58 +0530

    Add tasks route again

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Dashboard from "./pages/Dashboard";
 import Projects from "./pages/Projects";
-
+import Tasks from "./pages/Tasks";
 import KanbanBoard from "./pages/KanbanBoard";
 
 function App() {
@@ -15,7 +15,7 @@ function App() {
         <Route path="/signup" element={<Signup />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/projects" element={<Projects />} />
-        
+        <Route path="/tasks" element={<Tasks />} />
         <Route path="/kanban" element={<KanbanBoard />} />
       </Routes>
     </BrowserRouter>
